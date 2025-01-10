2cb87debd032d69a30fa1524fb45953a8842334f
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-01-10 12:29:29 -0300

    fix: API Mapping

--- main.tf
+++ main.tf
@@ -19,8 +19,9 @@ resource "aws_apigatewayv2_domain_name" "api" {
 }
 
 resource "aws_apigatewayv2_api_mapping" "api" {
+  count       = var.custom_domain == null ? 0 : 1
   api_id      = aws_apigatewayv2_api.api.id
-  domain_name = aws_apigatewayv2_domain_name.api[0].domain_name
+  domain_name = aws_apigatewayv2_domain_name.api[count.index].domain_name
   stage       = aws_apigatewayv2_stage.api.name
 }
 
